fcb24821f94215ee13fb79ae4645f6e316fe3994
Zack Argyle <zargyle@verisage.us>
2014-04-08 18:19:32 -0600

    Added push, splice, slice, move, clear, and data

--- xenon.js
+++ xenon.js
@@ -1,23 +1,17 @@
 'use strict';
 
-function Xenon (id, array, options) {
-
-	/* POSSIBLE OPTIONS
-		- filtering
-		- sorting
-		- limitTo
-		- startFrom
-		- paging
-	*/
+function Xenon (id, data) {
 
 	var element = document.getElementById(id),
 			parent  = element.parentNode,
 			display = element.style.display,
-			clones  = [];
+			nodes  = [], this_ = this;
 
-	function getValue(obj, array) {
-		for (var i = 0; i < array.length; i++) {
-			obj = obj[array[i]];
+	element.style.display = "none";
+
+	function getValue(obj, attrs) {
+		for (var i = 0; i < attrs.length; i++) {
+			obj = obj[attrs[i]];
 		}
 		return obj;
 	}
@@ -28,7 +22,7 @@ function Xenon (id, array, options) {
 			if (node.nodeName === "INPUT") {
 				node.value = getValue(obj, attr);
 			} else {
-				node.innerHTML = getValue(obj, attr);
+				node.innerHTML = getValue(obj, attr) + node.innerHTML;
 			}
 		}
 		if (node.getAttribute("x-class")) {
@@ -48,38 +42,67 @@ function Xenon (id, array, options) {
 		}
 	}
 
-	function addChild (val) {
+	function insert(obj, index) {
+		if (index === undefined) 
+			index = data.length;
+
 		var clone = element.cloneNode(true);
 		clone.removeAttribute("id");
-		setupChild(clone, val);
-		clones.push(clone);
-		parent.appendChild(clone);
+		setupChild(clone, obj);
+		clone.style.display = display;
+
+		parent.insertBefore(clone, nodes[index]);
+		nodes.splice(index, 0, clone);
+		data.splice(index, 0, obj);
+		return this_;
 	}
 
-	this.update = function(updatedArray) {
-		array = updatedArray;
-		for (var i = 0; i < clones.length; i++) {
-			parent.removeChild(clones[i]);
-		}
-		element.style.display = display;
-		clones = [];
-		for (var i = 0; i < array.length; i++) {
-			addChild(array[i]);
-		}
-		element.style.display = "none";
+	function remove(index) {
+		parent.removeChild(nodes[index]);
+		nodes.splice(index,1);
+		return data.splice(index,1)[0];
+	}
+
+	this.set = function(updatedData) {
+		for (var i = 0; i < nodes.length; i++)
+			parent.removeChild(nodes[i]);
+		nodes = [], data = [];
+		for (var i = 0; i < updatedData.length; i++)
+			insert(updatedData[i]);
+		return this;
 	}
 
 	this.push = function(obj) {
-		element.style.display = display;
-		addChild(obj);
-		element.style.display = "none";
+		return insert(obj);
 	}
 
-	if (array) this.update(array);
-}
+	this.splice = function(index, num, obj) {
+		while (num-- > 0) {
+			remove(index);
+		}
+		if (obj) return insert(obj, index);
+		else return this;
+	}
 
-// (function() {
-// 	var array = [{test1:"Test1.1", test2: "Test1.2"},{test1:"Test2.1", test2:"Test2.2"}];
-// 	var test = new X("repeat", array);
-// 	test.push({test1:"Test3.1", test2:"Test3.2"});
-// })();
+	this.slice = function(begin, end) {
+		return this.set(data.slice(begin, end));
+	}
+
+	this.move = function(from, to) {
+		var obj = remove(from);
+		if (from > to)
+			return insert(obj, to);
+		else
+			return insert(obj, to-1);
+	}
+
+	this.clear = function() {
+		return this.set([]);
+	}
+
+	this.data = function() {
+		return data;
+	}
+
+	this.set(data || []);
+}
